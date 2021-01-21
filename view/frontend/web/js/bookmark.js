81b7a2cee8c53d7c29cad727dbcc3c886501e7c3
IvanSoldo <ivandragosoldo@gmail.com>
2021-01-21 20:55:09 +0100

    minor fix

--- view/frontend/web/js/bookmark.js
+++ view/frontend/web/js/bookmark.js
@@ -1,10 +1,14 @@
 define(['jquery'], function ($) {
     'use strict';
+
     return function (config, element) {
-        $.get('/inchoo_bookmark/block', function (result) {
-            element.innerHTML = result;
-            let id = document.getElementById('product');
-            id.value = config.product;
+        $.get({
+            url: '/inchoo_bookmark/block',
+            success: function (result) {
+                element.innerHTML = result;
+                let productId = document.getElementById('product');
+                productId.value = config.product;
+            }
         });
     }
 });
